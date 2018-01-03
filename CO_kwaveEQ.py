b97466ae2bc61aa1e98be3eab75152656396225e
Sitterson <sitterson.jan@epa.gov>
2018-01-03 17:19:06 -0500

    Equation compare script and channel geom script

--- CO_kwaveEQ.py
+++ CO_kwaveEQ.py
@@ -11,7 +11,9 @@ import pandas as pd
 
 #state Variables
 model_segs=pd.DataFrame(data={'name':["BC_1", "BC_2", "BC_3", "BC_4", "BC_5", "BC_6"],
-                   'length':[9703, 6641, 5707, 9209, 5420, 10658], 
+                  'length':[9703, 6641, 5707, 9209, 5420, 10658], 
+#                                             'length':[9, 6, 5, 9, 5, 10], 
+  
                    'bot_width':[1.22, 2.1699, 2.660, 2.830, 3.8599, 3.9199],
                    'chan_slope':[0.001760, 0.00171, 0.00063, 0.00163, 0.00028, 0.001],
                    'mannings_n':[0.06, 0.12, 0.075, 0.115, 0.065, 0.12]})
@@ -22,38 +24,35 @@ s=model_segs.chan_slope
 w=model_segs.bot_width
 beta=3/5
 alpha=((n*w**(2/3))/s**0.5)**beta
-delta_t=360
-end_time=8514000
+delta_t=10
+end_time=17600
 #setup
 
 
 time2d=(np.arange(0,end_time,delta_t))
-Qboun2d=(1-np.cos((time2d*np.pi)/450))*(750/np.pi)+200 #np.atleast_2d
+Qboun2d=(1-np.cos((time2d*np.pi)/450))*(750/np.pi) #np.atleast_2d
 Qx=np.ones((len(time2d),len(x)+2),dtype=float) 
 Qout_CO=np.concatenate((time2d[:,None],Qboun2d[:,None],Qx), axis=1)
 Qout_Chap=np.concatenate((time2d[:,None],Qboun2d[:,None],Qx), axis=1)
-tt=[]
 
 
-for i in range(x.count()):
-    init_time=0
-    for j in range(len(time2d)-1):
-        this_time = Qout_CO[j][0]
-        dt=this_time - init_time
-        Qout_CO[j+1][i+2]=(delta_t/x[i]*Qout_CO[j+1][i+1] + alpha[i]*beta*Qout_CO[j][i+2]*((Qout_CO[j][i+1] + Qout_CO[j+1][i+1])/2)**(beta-1))/(delta_t/x[i] + alpha[i]*beta*((Qout_CO[j][i+2]+Qout_CO[j+1][i+1])/2)**(beta-1))
-        Qout_Chap[j+1][i+2]=Qout_Chap[j][i+2]+((Qout_Chap[j][i+1]-Qout_Chap[j][i+2])*(dt))/(x[i]*alpha[i]*beta*Qout_Chap[j][i+2]**(beta-1))
-        init_time = this_time
+for t in range(len(time2d)-1):
+   
+    for i in range(x.count()):
+        
+        Qout_CO[t+1][i+2]=((delta_t/x[i])*Qout_CO[t+1][i+1] + alpha[i]*beta*Qout_CO[t][i+2]*((Qout_CO[t][i+2] + Qout_CO[t+1][i+1])/2)**(beta-1))/(delta_t/x[i] + alpha[i]*beta*((Qout_CO[t][i+2]+Qout_CO[t+1][i+1])/2)**(beta-1))
+        Qout_Chap[t+1][i+2]=Qout_Chap[t][i+2]+((Qout_Chap[t][i+1]-Qout_Chap[t][i+2])*(delta_t))/(x[i]*alpha[i]*beta*Qout_Chap[t][i+2]**(beta-1))
+        
     
-    tt.append(dt)
-
+  
 #%%     Plotting Code     #######   
 
-plt.plot(Qout_CO[:,1], 'b', label='Boundary Flow')
-plt.plot(Qout_CO[:,2], 'c', label='FirstCO')
-plt.plot(Qout_Chap[:,2], 'g', label='FirstChap')
+#plt.plot(time2d,Qout_CO[:,1], 'b', label='Boundary Flow')
+plt.plot(time2d,Qout_CO[:,2], 'c', label='FirstCO')
+plt.plot(time2d,Qout_Chap[:,2], 'g', label='FirstChap')
 #plt.plot(Qout_CO[4], 'r', label='Third')
 #plt.plot(Qout_CO[5], 'k', label='Fourth')
 plt.ylabel('Flow')
 plt.title('Colorado and Chapra Kwave Comparison')
 plt.legend(loc='lower right')
-plt.axis([0,50,0,600])
+#plt.axis([0,360,0,700])
--- kwave.py
+++ kwave.py
@@ -10,39 +10,37 @@ import pandas as pd
 
 def perimeter_calc(shape, d,wide,z_slope):
     if shape =='tz':#for trapezoid
-        perim=wide+2*d*(1+z_slope**2)**0.5    #this may not be calculating correctly
+        perim=wide+2*d*(1+z_slope**2)**0.5    
     elif shape =='r':#for rectangle
         perim=2*d+wide
     elif  shape =='t':#for triangle
         perim=2*d*(1+z_slope**2)**0.5
     return perim
 
-#state Variables
+###################state Variables
 model_segs=pd.DataFrame(data={'name':["BC_1", "BC_2", "BC_3", "BC_4", "BC_5", "BC_6"],
                    'length':[9703, 6641, 5707, 9209, 5420, 10658], 
                    'bot_width':[1.22, 2.1699, 2.660, 2.830, 3.8599, 3.9199],
                    'chan_slope':[0.001760, 0.00171, 0.00063, 0.00163, 0.00028, 0.001],
                    'mannings_n':[0.06, 0.12, 0.075, 0.115, 0.065, 0.12], 
                    'initial_depth':[1.73, 2.3, 2.41, 5.58, 2.8, 3.06],
-                   'chan_geom':['r', 'r', 'r', 'r', 'r','r']})
+                   'chan_geom':['t', 'r', 'r', 'r', 'r','r']})
 length=model_segs.length
 n=model_segs.mannings_n
 slope=model_segs.chan_slope
 w=model_segs.bot_width
 delta_t=60
-end_time=600#8514000
-model_segs.insert(loc= 7,column='Perimeter',value=0)#allow_duplicates=True
+end_time=16000#8514000
+#model_segs.insert(loc= 7,column='Perimeter',value=0)#allow_duplicates=True
 
-
-##setup dataframes
-
+####################setup dataframes
 time2d=(np.arange(0,end_time,delta_t))
 Qboun2d=time2d+200#(1-np.cos((time2d*np.pi)/450))*(750/np.pi)+200 #time2d
 Qx=np.zeros((len(time2d),len(length)+1),dtype=float) 
 Qx[0,:]=[0.001]*(len(length)+1)              #initial flow conditions of 0.001 m3/s
 Qout=np.concatenate((Qboun2d[:,None],Qx), axis=1)
 
-alpha_data=pd.DataFrame(data=np.zeros((len(time2d), length.count())), columns=list(model_segs.name))
+alpha_data=pd.DataFrame(data=np.zeros((len(time2d)-1, length.count())), columns=list(model_segs.name))
 
 area=pd.DataFrame(data=np.zeros((len(time2d), length.count())), columns=list(model_segs.name))
 depth_data =pd.DataFrame(data=np.zeros((len(time2d), length.count())), columns=list(model_segs.name))
@@ -51,23 +49,19 @@ velocity=pd.DataFrame(data=np.zeros((len(time2d), length.count())), columns=list
 ##need to make the depth column in alpha_data be the initial depth. 
 depth_data.loc[0,:]=list(model_segs['initial_depth'])
 
-
-##Channel Geometry 
+####################Channel Geometry 
 beta=3/5
 z=2 #assuming trapezoid and triangle have a side slope of 1/2
 #for j in range(len(model_segs['chan_geom'])):
 #    if model_segs['chan_geom'][j] =='tz':#for trapezoid
-#        model_segs['Perimeter'][j]=w[j]+1*depth_data[j]*(1+z**2)**0.5    #this may not be calculating correctly
+#        model_segs['Perimeter'][j]=w[j]+1*depth_data[j]*(1+z**2)**0.5    
 #    elif model_segs['chan_geom'][j] =='r':#for rectangle
 #        model_segs['Perimeter'][j]=2*depth_data[j]+w[j]
 #    elif  model_segs['chan_geom'][j] =='t':#for triangle
 #        model_segs['Perimeter'][j]=2*depth_data[j]*(1+z**2)**0.5
 #print(model_segs.Perimeter)
 
-
-##Time and Space Flow Loop####
-
-
+####################Time and Space Flow Loop####
 for t in range(len(time2d)-1): 
 
     for i in range(length.count()):
@@ -79,7 +73,6 @@ for t in range(len(time2d)-1):
 
         area.iloc[t,i]=alpha_data.iloc[t,i]*(Qout[t+1][i+1])**beta
         depth_data.iloc[t+1,i]=area.iloc[t,i]/w[i]
-        #alpha_data.iloc[t,i]=alpha
         velocity.iloc[t,i]=Qout[t+1][i+1]/area.iloc[t,i]
         
 print(alpha_data.head(n=6))
@@ -89,13 +82,13 @@ print(area.head(n=6))
 #print(alpha_data[0:15]) # shows zeros for the last time step
 
 #%%
-##Plotting Code##
+####################Plotting Code##
 
-plt.plot(time2d,Qout[:100,0], 'b', label='Boundary Flow')
-plt.plot(time2d,Qout[:100,1], 'c', label='First')
-plt.plot(time2d,Qout[:100,2], 'g', label='Second')
-plt.plot(time2d,Qout[:100,3], 'r', label='Third')
-plt.plot(time2d,Qout[:100,4], 'k', label='Fourth')
+plt.plot(time2d,Qout[:,0], 'b', label='Boundary Flow')
+plt.plot(time2d,Qout[:,1], 'c', label='First')
+plt.plot(time2d,Qout[:,2], 'g', label='Second')
+plt.plot(time2d,Qout[:,3], 'r', label='Third')
+plt.plot(time2d,Qout[:,4], 'k', label='Fourth')
 plt.ylabel('Flow')
 
 #plt.axis([0,200,2000,2500])
@@ -103,7 +96,6 @@ plt.legend()
                 
 
 
-#print(np.subtract(Qout[4][2],Qout[4][8])) #using numpy functions is faster than python functions
 
 
 
