f01a1bf5c012ce0600796127c4bfef9c97d5ddcc
Sitterson <sitterson.jan@epa.gov>
2018-01-11 10:41:04 -0500

    Trying to fix WASP time step

--- CO_kwaveEQ.py
+++ CO_kwaveEQ.py
@@ -26,15 +26,15 @@ s=model_segs.chan_slope
 w=model_segs.bot_width
 beta=3/5
 alpha=((n*w**(2/3))/s**0.5)**beta
-delta_t=60
-end_time=10140
+delta_t=60 #seconds
+end_time=43320
 
 #setup
 
 
 time2d=(np.arange(0,end_time,delta_t))
-Qboun2d=np.full((len(time2d)),5)
-#(1-np.cos((time2d*np.pi)/450))*(750/np.pi)+2 #np.atleast_2d
+#np.full((len(time2d)),5)
+Qboun2d=(1-np.cos((time2d*np.pi)/450))*(750/np.pi)+2 #np.atleast_2d
 Qx=np.ones((len(time2d),len(x)),dtype=float) 
 Qout_CO=np.concatenate((time2d[:,None],Qboun2d[:,None],Qx), axis=1)
 Qout_Chap=np.concatenate((time2d[:,None],Qboun2d[:,None],Qx), axis=1)
@@ -50,36 +50,39 @@ for t in range(len(time2d)-1):
 
 
 #%%    WASP input    
-wasptime=range(0, 170,1)
+
 os.getcwd()
 os.chdir('C:/Users/jsitters/WASP/K_wave_Trial/')
-WASP_Output = pd.read_table('WASP_200F.txt', sep="\t", header=0) #this is not right... needs work on segment location
+WASP_Output = pd.read_table('sine_Flow.txt', sep="\t", header=0) #this is not right... needs work on segment location
 WASP_df = pd.DataFrame(WASP_Output)  
+wasptime=WASP_df.iloc[:,1]
+WASP_df.head
 
+wasptime.head
 #%%     Plotting Code     #######   
 plt.figure(1)
 #plt.plot(time2d/delta_t,Qout_CO[:,1], 'b', label='Boundary Flow')
-plt.plot(wasptime,WASP_df.iloc[:,1], 'r', label ='WASP_seg1')
+plt.plot(time2d/delta_t,WASP_df.iloc[:,2], 'r', label ='WASP_seg1')
 plt.plot(time2d/delta_t,Qout_CO[:,2], 'c', label='CO_seg1')
 plt.plot(time2d/delta_t,Qout_Chap[:,2], 'g', label='Chap_seg1')
 plt.ylabel('Flow')
 plt.xlabel('Time (min)')
 plt.title('Kwave Comparison')
 plt.legend(loc='lower right')
-plt.axis([0,100,0,5.5])
+#plt.axis([0,150,0,5.5])
 
 plt.figure(2)
-plt.plot(wasptime,WASP_df.iloc[:,2], 'r', label ='WASP_seg2')
+plt.plot(wasptime,WASP_df.iloc[:,3], 'r', label ='WASP_seg2')
 plt.plot(time2d/delta_t,Qout_CO[:,3], 'c', label='CO_seg2')
 plt.plot(time2d/delta_t,Qout_Chap[:,3], 'g', label='Chap_seg2')
 plt.ylabel('Flow')
 plt.xlabel('Time (min)')
 plt.title('Kwave Comparison')
 plt.legend(loc='lower right')
-plt.axis([0,100,0,5.5])
+plt.axis([0,150,0,5.5])
 
 plt.figure(3)
-plt.plot(wasptime,WASP_df.iloc[:,3], 'r', label ='WASP_seg3')
+plt.plot(wasptime,WASP_df.iloc[:,4], 'r', label ='WASP_seg3')
 plt.plot(time2d/delta_t,Qout_CO[:,4], 'c', label='CO_seg3')
 
 plt.plot(time2d/delta_t,Qout_Chap[:,4], 'g', label='Chap_seg3')
@@ -87,30 +90,30 @@ plt.ylabel('Flow')
 plt.xlabel('Time (min)')
 plt.title('Kwave Comparison')
 plt.legend(loc='lower right')
-plt.axis([0,100,0,5.5])
+plt.axis([0,150,0,5.5])
 
 plt.figure(4)
-plt.plot(wasptime,WASP_df.iloc[:,4], 'r', label ='WASP_seg4')
+plt.plot(wasptime,WASP_df.iloc[:,5], 'r', label ='WASP_seg4')
 plt.plot(time2d/delta_t,Qout_CO[:,5], 'c', label='CO_seg4')
 plt.plot(time2d/delta_t,Qout_Chap[:,5], 'g', label='Chap_seg4')
 plt.ylabel('Flow')
 plt.xlabel('Time (min)')
 plt.title('Kwave Comparison')
 plt.legend(loc='lower right')
-plt.axis([0,100,0,5.5])
+plt.axis([0,150,0,5.5])
 
 plt.figure(5)
-plt.plot(wasptime,WASP_df.iloc[:,5], 'r', label ='WASP_seg5')
+plt.plot(wasptime,WASP_df.iloc[:,6], 'r', label ='WASP_seg5')
 plt.plot(time2d/delta_t,Qout_CO[:,6], 'c', label='CO_seg5')
 plt.plot(time2d/delta_t,Qout_Chap[:,6], 'g', label='Chap_seg5')
 plt.ylabel('Flow')
 plt.xlabel('Time (min)')
 plt.title('Kwave Comparison')
 plt.legend(loc='lower right')
-plt.axis([0,100,0,5.5])
+plt.axis([0,150,0,5.5])
 
 plt.figure(6)
-plt.plot(wasptime,WASP_df.iloc[:,6], 'r', label ='WASP_seg6')
+plt.plot(wasptime,WASP_df.iloc[:,7], 'r', label ='WASP_seg6')
 plt.plot(time2d/delta_t,Qout_CO[:,7], 'c', label='CO_seg6')
 plt.plot(time2d/delta_t,Qout_Chap[:,7], 'g', label='Chap_seg6')
 
@@ -118,4 +121,11 @@ plt.ylabel('Flow')
 plt.xlabel('Time (min)')
 plt.title('Kwave Comparison')
 plt.legend(loc='lower right')
-plt.axis([0,100,0,5.5])
+plt.axis([0,150,0,5.5])
+
+plt.tight_layout()
+
+#%%
+plt.plot(wasptime,WASP_df.iloc[:,3], 'r', label ='WASP_seg6')
+plt.plot(time2d/delta_t,Qout_CO[:,4], 'c', label='CO_seg6')
+plt.axis([0,60,0,5.5])
