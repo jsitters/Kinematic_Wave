7410da262c8e464f202e73261292846792232bb4
Sitterson <sitterson.jan@epa.gov>
2018-01-12 15:57:01 -0500

    Updated all code and added SineWave

--- longKwave.py
+++ longKwave.py
@@ -26,13 +26,13 @@ w=model_segs.bot_width
 beta=3/5
 alpha=((n*w**(2/3))/s**0.5)**beta
 delta_t=60 #seconds
-end_time=1920
+end_time=7380
 
 #setup
 
 time=(np.arange(0,end_time,delta_t))
 #np.full((len(time)),5) #when you want a steady flow
-Qbound=(500*np.cos((time/3600)*2*np.pi-0.5*np.pi)) #sin wave
+Qbound=200*np.sin((time/3600)*2*np.pi-0.5*np.pi)+200#(500*np.cos((time/3600)*2*np.pi-0.5*np.pi)) #wave
 Qx=np.ones((len(time),len(x)),dtype=float) 
 Qout_CO=np.concatenate((time[:,None],Qbound[:,None],Qx), axis=1)
 Qout_Chap=np.concatenate((time[:,None],Qbound[:,None],Qx), axis=1)
@@ -51,11 +51,11 @@ for t in range(len(time)-1):
 
 os.getcwd()
 os.chdir('C:/Users/jsitters/WASP/K_wave_Trial/')
-WASP_Output = pd.read_table('minute_trial.txt', sep="\t", header=0) #this is not right... needs work on segment location
+WASP_Output = pd.read_table('longKwave.txt', sep="\t", header=0) #this is not right... needs work on segment location
 WASP_df = pd.DataFrame(WASP_Output)  
 wasptime=WASP_df.iloc[:,1]
 
-#%%     Plotting Code     #######   
+#%%     Individual Plotting Code     #######   
 plt.figure(1)
 plt.subplot(611)
 #plt.plot(time/delta_t,Qout_CO[:,1], 'b', label='Boundary Flow')
@@ -122,36 +122,37 @@ plt.legend(loc='lower right')
 
 plt.tight_layout()
 
-#%%
+#%%  Combined Plots
+
 fig,ax =plt.subplots(6, sharex=True, sharey=True)
-ax[0].plot(time/60,WASP_df.iloc[:,1], 'r', label ='WASP_seg1')
 ax[0].plot(time/60,Qout_CO[:,2], 'c', label='CO_seg1')
 ax[0].plot(time/60,Qout_Chap[:,2], 'b', label='Chap_seg1')
+ax[0].plot(time/60,WASP_df.iloc[:,1], 'r', label ='WASP_seg1')
 
-ax[1].plot(time/60,WASP_df.iloc[:,2], 'r', label ='WASP_seg2')
 ax[1].plot(time/60,Qout_CO[:,3], 'c', label='CO_seg2')
 ax[1].plot(time/60,Qout_Chap[:,3], 'b', label='Chap_seg2')
+ax[1].plot(time/60,WASP_df.iloc[:,2], 'r', label ='WASP_seg2')
 
-ax[2].plot(time/60,WASP_df.iloc[:,3], 'r', label ='WASP_seg3')
 ax[2].plot(time/60,Qout_CO[:,4], 'c', label='CO_seg3')
 ax[2].plot(time/60,Qout_Chap[:,4], 'b', label='Chap_seg3')
+ax[2].plot(time/60,WASP_df.iloc[:,3], 'r', label ='WASP_seg3')
 
-ax[3].plot(time/60,WASP_df.iloc[:,4], 'r', label ='WASP_seg4')
 ax[3].plot(time/60,Qout_CO[:,5], 'c', label='CO_seg4')
 ax[3].plot(time/60,Qout_Chap[:,5], 'b', label='Chap_seg4')
+ax[3].plot(time/60,WASP_df.iloc[:,4], 'r', label ='WASP_seg4')
 
-ax[4].plot(time/60,WASP_df.iloc[:,5], 'r', label ='WASP_seg5')
 ax[4].plot(time/60,Qout_CO[:,6], 'c', label='CO_seg5')
 ax[4].plot(time/60,Qout_Chap[:,6], 'b', label='Chap_seg5')
+ax[4].plot(time/60,WASP_df.iloc[:,5], 'r', label ='WASP_seg5')
 
-ax[5].plot(time/60,WASP_df.iloc[:,6], 'r', label ='WASP')
 ax[5].plot(time/60,Qout_CO[:,7], 'c', label='CO')
 ax[5].plot(time/60,Qout_Chap[:,7], 'b', label='Chap')
+ax[5].plot(time/60,WASP_df.iloc[:,6], 'r', label ='WASP')
 
-fig.suptitle('Kinematic Wave Comparison PI')
+fig.suptitle('Kinematic Wave Comparison 2PI')
 plt.legend(loc='center left')#, bbox_to_anchor=(1, 0.5))
 plt.ylabel('Flow (m3/s)')#, horizontalalignment='right')
 plt.xlabel('Time (min)')
 
-#plt.savefig('KinematicPi_Compare.png')
+#plt.savefig('Kinematic2PI_Compare.png')
 
